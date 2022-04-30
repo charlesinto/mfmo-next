7c5b936c280dfa50e94f00b4e86be819eca9468c
Charles Onuorah <charles.onuorah@yahoo.com>
2022-04-30 09:36:51 +0100

    fix address

--- pages/visit.tsx
+++ pages/visit.tsx
@@ -68,7 +68,7 @@ const Visit = () => {
               >
                 Maranatha Family Mission Outreach
               </p>
-              <p className="info mb-0">2 Amanze Osajie Street,</p>
+              <p className="info mb-0">2 Ameze Osagie Street,</p>
               <p className="info">
                 Golden Pearl Estate (opposite Happyland Estate) Olokonla, Ajah
                 Lekki Lagos
